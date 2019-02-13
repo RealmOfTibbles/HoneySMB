1c4f563eff332fffcc5b0fa2b3738e74d8356f09
RealmOfTibbles <5703945+RealmOfTibbles@users.noreply.github.com>
2019-02-13 14:01:25 +0000

    Added Restrictions to the docker container

--- Build-Quiet.sh
+++ Build-Quiet.sh
@@ -9,13 +9,13 @@ if [ ! -d "smbDrive" ]
 then
     mkdir smbDrive
 fi
-
-docker run --name SMB -d -p $1:445:445 -p $1:139:139 -v `pwd`/logs:/home/smb/logs/ -v `pwd`/smbDrive:/home/smb/smbDrive/ -i smbserver
+# can only use 1GB 75% CPU for one core when cpu is restricted its priority is lower then defult 
+docker run --memory=1024 --cpus="0.75" --cpu-shares=768 --name SMB -d -p $1:445:445 -p $1:139:139 -v `pwd`/logs:/home/smb/logs/ -v `pwd`/smbDrive:/home/smb/smbDrive/ -i smbserver
 
 if [ $? -ne 0 ]
 then
   echo "[*] Docker with name SMB already running"
   echo "[*] Stopping SMB and rerunning"
   docker rm -f SMB
-  docker run --name SMB -d -p $1:445:445 -p $1:139:139 -v `pwd`/logs:/home/smb/logs/ -v `pwd`/smbDrive:/home/smb/smbDrive/ -i smbserver
+  docker run --memory=1024 --cpus="0.75" --cpu-shares=768 --name SMB -d -p $1:445:445 -p $1:139:139 -v `pwd`/logs:/home/smb/logs/ -v `pwd`/smbDrive:/home/smb/smbDrive/ -i smbserver
 fi
