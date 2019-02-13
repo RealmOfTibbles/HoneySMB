001cee84f67d0d06a7fb963164e27e3726c5e6ce
RealmOfTibbles <5703945+RealmOfTibbles@users.noreply.github.com>
2019-02-13 13:47:50 +0000

    Update Build-Quiet.sh
    Streamlined it more

--- Build-Quiet.sh
+++ Build-Quiet.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-then
-  echo "[*] Please run as root"
-  exit
-fi
-
 docker build -t smbserver .
-
 if [ ! -d "logs" ]
 then
   mkdir logs
@@ -26,6 +19,3 @@ then
   docker rm -f SMB
   docker run --name SMB -d -p $1:445:445 -p $1:139:139 -v `pwd`/logs:/home/smb/logs/ -v `pwd`/smbDrive:/home/smb/smbDrive/ -i smbserver
 fi
-
-echo -e "[*]Setting up environment for extracting info from Log";
- 
