0e458acd85f75cdcd53cf69dd886106a921955a3
Harvey <5703945+RealmOfTibbles@users.noreply.github.com>
2019-02-15 15:52:15 +0000

    Build Quiet update

--- Build-Quiet.sh
+++ Build-Quiet.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 docker build -t smbserver .
-if [ ! -d "logs" ]
-then
-  mkdir logs
-fi
+#if [ ! -d "logs" ]
+#then
+#  mkdir /opt/Honeypot-SMB-HoneySMB/HoneySMB/logs
+#fi
 
-if [ ! -d "smbDrive" ]
-then
-    mkdir smbDrive
-fi
+#if [ ! -d "smbDrive" ]
+#then
+#    mkdir /opt/Honeypot-SMB-HoneySMB/HoneySMB/smbDrive
+#fi
 # can only use 0.5GB 75% CPU for one core when cpu is restricted its priority is lower then defult 
-docker run --memory=512m --cpus="0.75" --cpu-shares=768 --name SMB -d -p $1:445:445 -p $1:139:139 -v `pwd`/logs:/home/smb/logs/ -v `pwd`/smbDrive:/home/smb/smbDrive/ -i smbserver
+docker run --memory=512m --cpus="0.75" --cpu-shares=768 --name SMB -d -p $1:445:445 -p $1:139:139 -v /opt/Honeypot-SMB-HoneySMB/HoneySMB/logs:/home/smb/logs/ -v /opt/Honeypot-SMB-HoneySMB/HoneySMB/smbDrive:/home/smb/smbDrive/ -i smbserver
 
 if [ $? -ne 0 ]
 then
   echo "[*] Docker with name SMB already running"
   echo "[*] Stopping SMB and rerunning"
   docker rm -f SMB
-  docker run --memory=512m --cpus="0.75" --cpu-shares=768 --name SMB -d -p $1:445:445 -p $1:139:139 -v `pwd`/logs:/home/smb/logs/ -v `pwd`/smbDrive:/home/smb/smbDrive/ -i smbserver
+  docker run --memory=512m --cpus="0.75" --cpu-shares=768 --name SMB -d -p $1:445:445 -p $1:139:139 -v /opt/Honeypot-SMB-HoneySMB/HoneySMB/logs:/home/smb/logs/ -v /opt/Honeypot-SMB-HoneySMB/HoneySMB/smbDrive:/home/smb/smbDrive/ -i smbserver
 fi
+
+echo "[!] Done"
